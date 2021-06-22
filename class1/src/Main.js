e22c7718eb9ba60aa0aaf483e6792a6cefff7f5c
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-06-22 21:20:00 +0300

    lab3

--- class1/src/Main.js
+++ class1/src/Main.js
@@ -4,36 +4,26 @@ import HornedBeast from './HornedBeast'
 import hornsAnimalData from './hornsAnimalData.json'
 import {Container, CardGroup,Card,Row} from 'react-bootstrap'
 
-// const styles = {
-//     card: {
-//       backgroundColor: '#B7E0F2',
-//       borderRadius: 55,
-//       padding: '3rem'
-//     },
-//     cardImage: {
-//       objectFit: 'cover',
-//       borderRadius: 55
-//     }
-//   }
+
 class Main extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  
     
     render() {
         return ( 
             // <Container xs={4} fluid>
             // <CardGroup className="m-5 d-block">
+
             //   <Card className="m-5 border-0 shadow" style={styles.card}>
             <Row>
                 {
-                    hornsAnimalData.map(item => {
+                    this.props.data.map(item => {
                         return (
                             <HornedBeast
                                 title={item.title}
                                 img_url={item.image_url}
                                 description={item.description}
+                                modal={this.props.selectedmodal}
                             
                             />
                         )
