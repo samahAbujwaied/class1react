5dd4d9ed8b1c24606df090b1c81b3114417aa8ae
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-06-23 23:51:21 +0300

    lab4

--- class1/src/Main.js
+++ class1/src/Main.js
@@ -2,37 +2,41 @@
 import React from 'react';
 import HornedBeast from './HornedBeast'
 import hornsAnimalData from './hornsAnimalData.json'
-import {Container, CardGroup,Card,Row} from 'react-bootstrap'
-
+import { Container, CardGroup, Card, Row } from 'react-bootstrap'
+import FilterForm from './FilterForm'
 
 class Main extends React.Component {
 
-  
-    
-    render() {
-        return ( 
-            // <Container xs={4} fluid>
-            // <CardGroup className="m-5 d-block">
 
-            //   <Card className="m-5 border-0 shadow" style={styles.card}>
-            <Row>
-                {
-                    this.props.data.map(item => {
-                        return (
-                            <HornedBeast
-                                title={item.title}
-                                img_url={item.image_url}
-                                description={item.description}
-                                modal={this.props.selectedmodal}
-                            
-                            />
-                        )
-                    })
-                }
-            </Row>
-    //         </Card>
-    //   </CardGroup>
-    // </Container>
+    render() {
+        return (
+            <>
+            
+            <Container xs={4} fluid>
+                <CardGroup className="m-5 d-block">
+
+                    <Card className="m-5 border-0 shadow">
+                        <Row>
+                            {
+                                this.props.data.map(item => {
+                                    return (
+                                        <HornedBeast
+                                            title={item.title}
+                                            img_url={item.image_url}
+                                            description={item.description}
+                                            modal={this.props.selectedmodal}
+
+                                        />
+                                    )
+                                })
+                            }
+                        </Row>
+
+                    </Card>
+                </CardGroup>
+            </Container>
+           
+            </>
         )
     }
 
